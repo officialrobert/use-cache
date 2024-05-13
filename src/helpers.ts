ce774d9b87e94bbebfe721ca088a4d33ad1db5b3
officialrob <officialrobert.espina9627@gmail.com>
2024-05-13 09:31:40 +0800

    chore: checkRedis() jsdoc

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,6 +1,11 @@
 import { UseCacheError } from './errors';
 import { store } from './store';
 
+/**
+ * Check both redis(ioredis & upstash) instance reference.
+ * Throws an error if neither is present.
+ * @returns 
+ */
 export const checkRedis = (): boolean => {
   if (!store?.redis && !store?.upstashRedis) {
     throw new UseCacheError('Missing redis instance');
